75b3d27bb5036542af3a29a4656e86534d23eb91
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-04-10 23:23:41 +0300

    0001: 004_tetris

---
+++ tetris_from_scratch/004_tetris.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt5 tutorial
+
+This example shows an icon
+in the titlebar of the window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.resize(250, 150)
+        self.move(300, 300)
+        self.setWindowTitle('Tetris')
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
