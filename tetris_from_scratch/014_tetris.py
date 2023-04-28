57652b0095684720bbb0b75392aa0e9ca572c714
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-04-28 16:58:09 +0300

    0010: 014_tetris red rectangle

--- tetris_from_scratch/014_tetris.py
+++ tetris_from_scratch/014_tetris.py
@@ -13,6 +13,7 @@ Website: zetcode.com
 import sys
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QFrame
+from PyQt5.QtGui import QPainter, QColor, QBrush
 
 
 class Tetris(QMainWindow):
@@ -56,6 +57,20 @@ class Board(QFrame):
 
     def start(self):
         self.msg2Statusbar.emit("Ready")
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawRectangles(qp)
+        qp.end()
+
+    def drawRectangles(self, qp):
+        col = QColor(0, 0, 0)
+        col.setNamedColor('#d4d4d4')
+        qp.setPen(col)
+
+        qp.setBrush(QColor(200, 0, 0))
+        qp.drawRect(10, 15, 90, 60)
 
 
 def main():
